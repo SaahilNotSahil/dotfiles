f719bcf9f89bda932694445767f0c3b9f32d3cc0
XanderWatson <saahil_bhavsar@outlook.com>
2022-01-19 01:39:09 +0530

    Added postinstall script for ubuntu

---
+++ ubuntu/postInstall.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+echo "Welcome to the Ubuntu Post Install Script!"
+echo "                           -authored by XanderWatson"
+echo "Let's get you started!"
+
+sudo apt update && sudo apt upgrade -y
+sudo apt install -y git vim gcc g++ make cmake gdb llvm clang bison flex build-essential
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb -O ~/Downloads/chrome.deb
+sudo dpkg -i ~/Downloads/chrome.deb
+wget https://az764295.vo.msecnd.net/stable/899d46d82c4c95423fb7e10e68eba52050e30ba3/code_1.63.2-1639562499_amd64.deb -O ~/Downloads/code.deb
+sudo dpkg -i ~/Downloads/code.deb
+sudo apt install -y python3-pip pipenv
+sudo snap install discord
+echo "export PATH='~/.local/bin:$PATH'" >> ~/.bashrc
+source ~/.bashrc
+
