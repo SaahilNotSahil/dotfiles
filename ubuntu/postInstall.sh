e4a4e62281dc01d4c7713df2ecab911072de3d7a
Saahil Bhavsar <saahil_bhavsar@outlook.com>
2023-05-05 18:53:01 +0530

    Updates postinstall scripts

--- ubuntu/postInstall.sh
+++ ubuntu/postInstall.sh
@@ -5,14 +5,12 @@ echo "                           -authored by XanderWatson"
 echo "Let's get you started!"
 
 sudo apt update && sudo apt upgrade -y
-sudo apt install -y git vim gcc g++ make cmake gdb llvm clang bison flex build-essential
+sudo apt install -y git vim neovim gcc g++ make cmake gdb llvm clang bison flex build-essential neofetch htop
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb -O ~/Downloads/chrome.deb
 sudo dpkg -i ~/Downloads/chrome.deb
 wget https://az764295.vo.msecnd.net/stable/899d46d82c4c95423fb7e10e68eba52050e30ba3/code_1.63.2-1639562499_amd64.deb -O ~/Downloads/code.deb
 sudo dpkg -i ~/Downloads/code.deb
 sudo apt install -y python3-pip
-sudo apt upgrade -y
-sudo snap install discord
-echo "export PATH='~/.local/bin:$PATH'" >> ~/.bashrc
+sudo apt update && sudo apt upgrade -y
+echo "export PATH='~/.local/bin:$PATH'" >>~/.bashrc
 source ~/.bashrc
-
